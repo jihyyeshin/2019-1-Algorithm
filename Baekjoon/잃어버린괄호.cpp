26dcc2f9e6267034d1d49a3d27fcb671ba365347
jihyyeshin <tlsjh082@gmail.com>
2019-09-05 23:32:48 +0900

    잃어버린 괄호 implemented

---
+++ Baekjoon/잃어버린괄호.cpp
@@ -0,0 +1,48 @@
+/*
+식에 괄호를 적절히 쳐서 식의 값을 최소로 만드는 문제
+: -가 나오면, 항상 뒤의 식을 -로 만들 수 있음: 이 때 최소가 됨
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+	string s;
+	cin >> s;
+	vector<int> num;
+	vector<int> sign;
+	bool minus = false;
+	int cur = 0;
+	sign.push_back(1);
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == '+' || s[i] == '-') {
+			if (s[i] == '+') {
+				sign.push_back(1);
+			}
+			else {
+				sign.push_back(-1);
+			}
+			num.push_back(cur);
+			cur = 0;
+		}
+		else {
+			cur = cur * 10 + (s[i] - '0');
+		}
+	}
+	num.push_back(cur);
+	int ans = 0;
+	minus = false;
+	for (int i = 0; i < num.size(); i++) {
+		if (sign[i] == -1) {
+			minus = true;
+		}
+		if (minus) {
+			ans -= num[i];
+		}
+		else {
+			ans += num[i];
+		}
+	}
+	cout << ans << '\n';
+	return 0;
+}

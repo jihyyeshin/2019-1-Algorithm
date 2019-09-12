c9286f0bf5a03c635c8252d2de6b1677fd18f456
jihyyeshin <tlsjh082@gmail.com>
2019-09-12 23:13:05 +0900

    톱니바퀴 implemented

--- Baekjoon/톱니바퀴.cpp
+++ Baekjoon/톱니바퀴.cpp
@@ -1,8 +1,12 @@
-
 #include <vector>
 #include <iostream>
 #include <string>
 using namespace std;
+
+void changeOne(string* t, int changeR, bool* checked);
+void changeTwo(string* t, int changeR, bool* checked);
+void changeThr(string* t, int changeR, bool* checked);
+void changeFr(string* t, int changeR, bool* checked);
 
 string sChange(string a) {// 시계방향으로 회전
 	string result = a;
@@ -21,159 +25,121 @@ string bChange(string a) {// 반시계방향으로 회전
 	return result;
 }
 
-/*
-잘못된 점
-- 1, 2, 3, 4번 톱니가 회전하는 것을 각각의 메소드로 구현하는 것이 좋다.
-각각 독립적으로 !
-*/
+void changeOne(string* t, int changeR, bool* checked) {
+	if (checked[0] == false) {
+		checked[0] = 1;
+		if (changeR == 1) {// 시계방향
+			if (t[0][2] != t[1][6])
+				changeTwo(t, -1, checked);
+			t[0] = sChange(t[0]);
 
+		}
+		else {// 반시계
+			if (t[0][2] != t[1][6])
+				changeTwo(t, 1, checked);
+			t[0] = bChange(t[0]);
+		}
+	}
+}
 
-//int main() {
-//	string t1;
-//	string t2;
-//	string t3;
-//	string t4;
-//	int result = 0;
-//	vector<bool> isDif;
-//	//vector<int, int> change;
-//	int n, buf, buf2;
-//	// 입력 및 초기화
-//	cin >> t1 >> t2 >> t3 >> t4;
-//	cin >> n;
-//	int *changeNum = new int[n];// 회전할 애
-//	int *changeR = new int[n];// 회전 방향
-//	for (int i = 0; i < n; i++) {
-//		cin >> changeNum[i] >> changeR[i];
-//	}
-//	for (int i = 0; i < 3; i++)
-//		isDif.push_back(false);
-//	// 입력 완료
-//	for (int i = 0; i < n; i++) {
-//		// 다른 애들을 true로 만들어둠
-//		if (t1[2] != t2[6]) isDif[0] = true;
-//		else if (t2[2] != t3[6]) isDif[1] = true;
-//		else if (t3[2] != t4[6]) isDif[2] = true;
-//		// 입력받은 사항에 따라 회전
-//		if (changeNum[i] == 1) { // num=1
-//			if (changeR[i] == 1) { //시계
-//				t1 = sChange(t1);
-//				if (isDif[0]) {
-//					t2 = bChange(t2);
-//					if (isDif[1]) {
-//						t3 = sChange(t3);
-//						if (isDif[2]) t4 = bChange(t4);
-//					}
-//				}
-//			}
-//			else { // 반시계
-//				t1 = bChange(t1);
-//				if (isDif[0]) {
-//					t2 = sChange(t2);
-//					if (isDif[1]) {
-//						t3 = bChange(t3);
-//						if (isDif[2]) t4 = sChange(t4);
-//					}
-//				}
-//			}
-//		}
-//		else if (changeNum[i] == 2) { // num=2
-//			if (changeR[i] == 1) { //시계
-//				t2 = sChange(t2);
-//				if (isDif[0]) 
-//					t1 = bChange(t1);
-//				if (isDif[1]) {
-//					t3 = bChange(t3);
-//					if (isDif[2])
-//						t4 = sChange(t4);
-//				}
-//			}
-//			else { // 반시계
-//				t2 = bChange(t2);
-//				if (isDif[0])
-//					t1 = sChange(t1);
-//				if (isDif[1]) {
-//					t3 = sChange(t3);
-//					if (isDif[2])
-//						t4 = bChange(t4);
-//				}
-//			}
-//		}
-//		else if (changeNum[i] == 3) { // num=3
-//			if (changeR[i] == 1) { //시계
-//				t3 = sChange(t3);
-//				if (isDif[2])
-//					t4 = bChange(t4);
-//				if (isDif[1]) {
-//					t2 = bChange(t2);
-//					if(isDif[0]) t1 = sChange(t1);
-//				}
-//			}
-//			else { // 반시계
-//				t3 = bChange(t3);
-//				if (isDif[2])
-//					t4 = sChange(t4);
-//				if (isDif[1]) {
-//					t2 = sChange(t2);
-//					if (isDif[0]) t1 = bChange(t1);
-//				}
-//			}
-//		}
-//		else { // num=4
-//			if (changeR[i] == 1) { //시계
-//				t4 = sChange(t4);
-//				if (isDif[2]) {
-//					t3 = bChange(t3);
-//					if (isDif[1]) {
-//						t2 = sChange(t2);
-//						if(isDif[0]) t1 = bChange(t1);
-//					}
-//				}
-//			}
-//			else { // 반시계
-//				t4 = bChange(t4);
-//				if (isDif[2]) {
-//					t3 = sChange(t3);
-//					if (isDif[1]) {
-//						t2 = bChange(t2);
-//						if (isDif[0]) t1 = sChange(t1);
-//					}
-//				}
-//			}
-//		}
-//
-//		for (int i = 0; i < 3; i++)
-//			isDif[i] = false;// 초기화
-//	}
-//
-//	if (t1[0] == '1') result += 1;
-//	if (t2[0] == '1') result += 2;
-//	if (t3[0] == '1') result += 4;
-//	if (t4[0] == '1') result += 8;
-//
-//	cout << result << '\n';
-//	return 0;
-//}
+void changeTwo(string* t, int changeR, bool* checked) {
+	if (checked[1] == false) {
+		checked[1] = 1;
+		if (changeR == 1) {
+			if (t[0][2] != t[1][6])
+				changeOne(t, -1, checked);
+			if (t[1][2] != t[2][6])
+				changeThr(t, -1, checked);
+			t[1] = sChange(t[1]);
+		}
+		else {
+			if (t[0][2] != t[1][6])
+				changeOne(t, 1, checked);
+			if (t[1][2] != t[2][6])
+				changeThr(t, 1, checked);
+			t[1] =bChange(t[1]);
+		}
+	}
+}
 
+void changeThr(string* t, int changeR, bool* checked) {
+	if (checked[2] == false) {
+		checked[2] = 1;
+
+		if (changeR == 1) {
+			if (t[2][2] != t[3][6])
+				changeFr(t, -1, checked);
+			if (t[1][2] != t[2][6])
+				changeTwo(t, -1, checked);
+			t[2] = sChange(t[2]);
+		}
+		else {
+			if (t[2][2] != t[3][6])
+				changeFr(t, 1, checked);
+			if (t[1][2] != t[2][6])
+				changeTwo(t, 1, checked);
+			t[2] = bChange(t[2]);
+		}
+	}
+}
+
+void changeFr(string* t, int changeR, bool* checked) {
+	if (checked[3] == false) {
+		checked[3] = 1;
+		if (changeR == 1) {// 시계방향
+			if (t[2][2] != t[3][6])
+				changeThr(t, -1, checked);
+			t[3] = sChange(t[3]);
+
+		}
+		else {// 반시계
+			if (t[2][2] != t[3][6])
+				changeThr(t, 1, checked);
+			t[3] = bChange(t[3]);
+		}
+	}
+}
 
 int main() {
-	string t1;
-	string t2;
-	string t3;
-	string t4;
+	string t[4];
 	int result = 0;
-	vector<bool> isDif;
+	bool checked[4] = { 0, };//이미 한 번 돌았는지 확인하기 위한 bool
 	int n, buf, buf2;
 	// 입력 및 초기화
-	cin >> t1 >> t2 >> t3 >> t4;
+	for (int i = 0; i < 4; i++)
+		cin >> t[i];
 	cin >> n;
 	int *changeNum = new int[n];// 회전할 애
 	int *changeR = new int[n];// 회전 방향
 	for (int i = 0; i < n; i++) {
 		cin >> changeNum[i] >> changeR[i];
 	}
-	for (int i = 0; i < 3; i++)
-		isDif.push_back(false);
-	// 입력 완료
 
+	for (int i = 0; i < n; i++) // 회전
+	{
+		if (changeNum[i] == 1) {
+			changeOne(t, changeR[i], checked);
+		}
+		else if (changeNum[i] == 2) {
+			changeTwo(t, changeR[i], checked);
+		}
+		else if (changeNum[i] == 3) {
+			changeThr(t, changeR[i], checked);
+		}
+		else {// changeNum==4
+			changeFr(t, changeR[i], checked);
+		}
+
+		// 초기화
+		for (int j = 0; j < 4; j++)
+			checked[j] = 0;
+	}
+
+	if (t[0][0] == '1') result += 1;
+	if (t[1][0] == '1') result += 2;
+	if (t[2][0] == '1') result += 4;
+	if (t[3][0] == '1') result += 8;
+
+	cout << result << '\n';
 	return 0;
 }

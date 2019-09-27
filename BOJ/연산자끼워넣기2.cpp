169d6653dfb75399d1516cc08c81f33ac15fd2a6
jihyyeshin <tlsjh082@gmail.com>
2019-09-27 16:15:33 +0900

    implemented

---
+++ BOJ/연산자끼워넣기2.cpp
@@ -0,0 +1,42 @@
+// 같은 문제, 재귀로 풀기
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+// 최대, 최소값을 모두 구하기 위해 두 개의 값 return
+pair<int, int> calc(vector<int> &a, int index,int cur, int plus, int minus, int mul, int div) {
+	int n = a.size();
+	if (index == n)
+		return make_pair(cur, cur);
+	vector<pair<int, int>> res;
+	if (plus > 0)
+		res.push_back(calc(a, index + 1, cur + a[index], plus - 1, minus, mul, div));
+	if(minus>0)
+		res.push_back(calc(a, index + 1, cur + a[index], plus, minus-1, mul, div));
+	if(mul>0)
+		res.push_back(calc(a, index + 1, cur + a[index], plus, minus, mul-1, div));
+	if(div>0)
+		res.push_back(calc(a, index + 1, cur + a[index], plus, minus, mul, div-1));
+	auto ans = res[0];
+	// 최대, 최소 구하는 과정
+	for (auto p : res) {
+		if (ans.first < p.first)
+			ans.first = p.first;
+		if (ans.second > p.second)
+			ans.second = p.second;
+	}
+	return ans;
+}
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	int plus, minus, mul, div;
+	cin >> plus >> minus >> mul >> div;
+	auto p = calc(a, 1, a[0], plus, minus, mul, div);
+	cout << p.first << '\n';
+	cout << p.second << '\n';
+	return 0;
+}
